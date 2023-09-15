04bea1e8faea6b0a0f4cb03973123a9a0f43f28b
flemerrer <francois.lemerrer@pm.me>
2023-09-15 14:56:40 +0200

    installed & setup routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import React from "react";
+import {Link} from "@mui/material";
 
-function App() {
+export default function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <Routes>
+            <Route path='/' element={<Home/>}>
+                {/*<Route path='login' element={<Register/>}/>*/}
+                {/*<Route path='login' element={<Login/>}/>*/}
+            </Route>
+            <Route path='/*' element={<NoMatch/>}/>
+        </Routes>
+
     </div>
-  );
+);
 }
 
-export default App;
+function Home(){
+    return(
+        <>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </>
+    )
+}
+
+function NoMatch() {
+    return (
+        <div>
+            <h2>Nothing to see here!</h2>
+            <p>
+                <Link href='/'>Go to the home page</Link>
+            </p>
+        </div>
+    );
+}
