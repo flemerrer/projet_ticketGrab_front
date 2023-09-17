bffca96c2a341aa051c5b7dae9f90b4d9d499c04
flemerrer <francois.lemerrer@pm.me>
2023-09-17 13:04:19 +0200

    starting filters

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import {Route, Routes} from "react-router-dom";
-import React from "react";
-import {Link} from "@mui/material";
+import {Outlet, Route, Routes} from 'react-router-dom';
+import React from 'react';
+import {Link} from '@mui/material';
+import EventList from "./components/EventList";
 
 export default function App() {
   return (
 
-    <div className="App">
+    <div className='App'>
 
         <Routes>
-            <Route path='/' element={<Home/>}>
+            <Route path='/'>
+                <Route path='home' element={<Home/>} />
                 {/*<Route path='login' element={<Register/>}/>*/}
                 {/*<Route path='login' element={<Login/>}/>*/}
+                <Route path='eventlist' element={<EventList />} />
             </Route>
             <Route path='/*' element={<NoMatch/>}/>
         </Routes>
@@ -24,18 +27,28 @@ export default function App() {
 function Home(){
     return(
         <>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
                 <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
+                    className='App-link'
+                    href='https://reactjs.org'
+                    target='_blank'
+                    rel='noopener noreferrer'
                 >
                     Learn React
                 </a>
             </header>
         </>
+    )
+}
+
+
+function Layout() {
+    return (
+        <div>
+
+            {/*<Outlet />*/}
+        </div>
     )
 }
 
---
+++ src/components/FilterButton.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Divider from '@mui/material/Divider';
+// import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
