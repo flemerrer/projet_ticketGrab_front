c8da2d6508e7f390859ae739ef96470e6a1e7589
Rodrigue Yedji <yedjirodrigue@yahoo.fr>
2023-09-24 09:13:10 +0200

    mise a jour de la branche, bur de mon ordi et de git

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 import {
     Button,
@@ -13,6 +14,7 @@ import {
     Typography
 } from "@mui/material";
 import axios from "axios";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function Tickets() {
 
@@ -33,45 +35,44 @@ export default function Tickets() {
 
     return (
         <>
-            <body className="App-headerT">
-                <h4>Tickets disponibles</h4>
+            <Container sx={{py: 8}} >
+                <Grid container spacing={4}>
+                    { tickets.map((ticket) => (
 
-                <Container sx={{py: 8}} >
-                    <Grid container spacing={4}>
-                        { tickets.map((ticket) => (
-
-                                <Grid item key={ticket.id}>
-                                    <Card sx={{maxWidth:260}}>
-                                        <CardMedia
-                                            component="img"
-                                            height="250"
-                                            sx={{height: 250, width: 260}}
-                                            image={ticket.img}
-                                        />
-                                        <CardContent>
-                                            <Typography variant="body2" color="text.secondary">
-                                                {ticket.id}
-                                            </Typography>
-                                            <Typography gutterBottom variant="h5" component="div">
-                                                {ticket.name}
-                                            </Typography>
-                                            <Typography gutterBottom variant="h5" component="div">
-                                                {ticket.event}
-                                            </Typography>
-                                        </CardContent>
-                                        <CardActions>
-                                            <Button size="small" variant="contained">Add to Cart</Button>
-                                            <Button size="small"  variant="outlined" color="error" onClick={() => deleteTicket(ticket.id)}>Delete</Button>
-                                        </CardActions>
-                                    </Card>
-                                </Grid>
-                            )
-                        )}
-                    </Grid>
-                </Container>
-
-            <footer></footer>
-            </body>
+                            <Grid item key={ticket.id}>
+                                <Card sx={{maxWidth:260}}>
+                                    <CardMedia
+                                        component="img"
+                                        height="250"
+                                        sx={{height: 250, width: 260}}
+                                        image={ticket.img}
+                                    />
+                                    <CardContent>
+                                        <Typography variant="body2" color="text.secondary">
+                                            {ticket.id}
+                                        </Typography>
+                                        <Typography gutterBottom variant="h7" component="div">
+                                            {ticket.name}
+                                        </Typography>
+                                        <Typography gutterBottom variant="h6" component="div" color="darkblue">
+                                            {ticket.event}
+                                        </Typography>
+                                        <Typography gutterBottom variant="h4" component="div" color="red">
+                                            {ticket.price} €
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <IconButton color="primary" aria-label="add to shopping cart">
+                                            <AddShoppingCartIcon />
+                                        </IconButton>
+                                        <Button size="small"  variant="outlined" startIcon={<DeleteIcon/>} color="error" onClick={() => deleteTicket(ticket.id)}>Delete</Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        )
+                    )}
+                </Grid>
+            </Container>
         </>
     );
 }
